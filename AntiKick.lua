9816c5fb1f600691ac6f441f1b8c612c8668f774
sof <swagatamsarkar521@gmail.com>
2025-02-03 14:51:22 +0000

    Update AntiKick.lua

--- AntiKick.lua
+++ AntiKick.lua
@@ -1,1 +1,39 @@
+assert(not getgenv().AntiKickExecuted, "Anti Kick is already running!")
 
+local Players = game:GetService("Players")
+
+-- // Vars (me)
+local LocalPlayer = Players.LocalPlayer
+local KickAttemptFormat = "Kick attempted for: %s"
+local BlockedMethods = {"Kick", "kick"}
+
+-- // What to do when Kick detected
+local function kickDetected(Player, Reason)
+    warn(KickAttemptFormat:format(Reason))
+end
+
+-- // Index hook
+hookfunction(LocalPlayer.Kick, kickDetected)
+
+-- // Namecall hook
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(self, ...)
+    -- // Vars (me pt. 2)
+    local args = {...}
+    local method = getnamecallmethod()
+    
+    -- // Check if kick attempt
+    if (self == LocalPlayer and table.find(BlockedMethods, method)) then
+        -- // Alert
+        kickDetected(self, args[1])
+        
+        -- // Cancel it
+        return
+    end
+    
+    -- // Return - so everything else that uses namecall does not break
+    return __namecall(self, ...)
+end)
+
+-- // the end
+getgenv().AntiKickExecuted = true
